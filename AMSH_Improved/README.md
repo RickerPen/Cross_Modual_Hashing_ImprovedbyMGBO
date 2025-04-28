d41b9f7fe90f231582c8616f23418f2a91d57cb3
RickerPen <2990271373@qq.com>
2025-04-28 22:10:38 +0800

    Delete AMSH_Improved directory

--- AMSH_Improved/README.md
+++
@@ -1,2 +0,0 @@
-# AMSH
-a demo for paper "Adaptive Marginalized Semantic Hashing for Unpaired Cross-Modal Retrieval"
--- AMSH_Improved/loaddata.m
+++
@@ -1,9 +0,0 @@
-function [LTrain, LTest,XTrain,XTest,YTrain,YTest] = loaddata(dataset)
-    load(dataset);
-    LTrain = L_tr;
-    LTest = L_te;
-    XTrain = I_tr;
-    YTrain = T_tr;
-    XTest = I_te;
-    YTest = T_te;
-end
